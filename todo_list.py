569ee9bcbcbd835fcfb12026c80f1199968c30cd
Fivescodes <ismael.isemann@gmail.com>
2024-10-20 14:31:46 +0200

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,7 @@
+echo "# TO-DO" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Fivescodes/TO-DO.git
+git push -u origin main
